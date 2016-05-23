9fdeef46ec4fa37d77c210feb61073c02acab00b
hpaasch <hepaasch@gmail.com>
2016-05-23 17:54:38 -0400

    Converted to Fahrenheit.

--- comprehensions_normal.py
+++ comprehensions_normal.py
@@ -1,50 +1,88 @@
 # -----------------------------------
 # Remove all vowels from the sentence
 
-# with a flabby FOR loop
-text = "List Comprehensions are the Greatest!"
-vowels = ("a", "e", "i", "o", "u")
+def flabby_loop_vowels():
+    text = "List Comprehensions are the Greatest!"
+    vowels = ("a", "e", "i", "o", "u")
 
-for letter in text:
-    if letter in vowels:
-        text = text.replace(letter, "")
-print(text)
+    for letter in text:
+        if letter in vowels:
+            text = text.replace(letter, "")
+    # print(text)
+    return text
+flabby_loop_vowels()
 
-# with a svelte List Comprehension
-text = "List Comprehensions are the Greatest!"
-vowels = ("a", "e", "i", "o", "u")
-text = [letter for letter in text if letter not in vowels]
-print("".join(text))
+def svelte_comprehension_vowels():
+    text = "List Comprehensions are the Greatest!"
+    vowels = ("a", "e", "i", "o", "u")
+    text = [letter for letter in text if letter not in vowels]
+    new_text = "".join(text)
+    # print(new_text)
+    return new_text
+svelte_comprehension_vowels()
 
 # ------------------------------------------------------------
 # Create a list of Water Temps for each day the data set below.
 
-import csv
-with open("data_set.txt") as water_data:
-    read_water_data = csv.reader(water_data, delimiter=',')
-    next(read_water_data, None)
-    dates = []
-    water_temps = []
+def collect_water():
+    import csv
+    with open("data_set.txt") as water_data:
+        read_water_data = csv.reader(water_data, delimiter=',')
+        next(read_water_data, None)
+        water_temps = []
 
-    for row in read_water_data:
-        date = row[-1]
-        water_temp = row[-2]
-        dates.append(date)
-        water_temps.append(water_temp)
-    print(dates)
-    print(water_temps)
+        for row in read_water_data: # change to comprehension
+            water_temp = row[-2]
+            water_temps.append(water_temp)
+        # print(water_temps)
+        return water_temps
+collect_water()
+
+def collect_dates():
+    import csv
+    with open("data_set.txt") as water_data:
+        read_water_data = csv.reader(water_data, delimiter=',')
+        next(read_water_data, None) # skips the header
+        dates = []
+
+        for row in read_water_data: # change to comprehension
+            date = row[-1]
+            dates.append(date)
+        # print(dates)
+        return dates
+collect_dates()
 
 # ------------------------------------------------
 # Convert the Water Temps from a string to a float
+
+def float_those_temperatures():
     floated_water_temps = []
 
-    for temperature in water_temps:
+    for temperature in collect_water():  # change to comprehension
             floated_temp = float(temperature)
             floated_water_temps.append(floated_temp)
-    print(floated_water_temps)
+
+    # print(floated_water_temps)
+    return floated_water_temps
+float_those_temperatures()
 
 
 # Convert the Water Temps from Celsius to Fahrenheit rounded to an int
+
+def convert_temps():
+    farenheit_water_temps =[]
+
+    for item in float_those_temperatures():
+        conversion_temp = int((item * 1.8) + 32)
+        farenheit_water_temps.append(conversion_temp)
+    print(farenheit_water_temps)
+    return farenheit_water_temps
+convert_temps()
+
+
 # Create a dictionary with Date as the key and Wave Height as the value
+
+
+
 # Create a dictionary with the average wave height for each day
 
