cfc47ff755bc77241f00da485862ed38a6a68492
hpaasch <hepaasch@gmail.com>
2016-05-23 22:55:53 -0400

    Partial on Homework 1

--- comprehensions_normal.py
+++ comprehensions_normal.py
@@ -98,14 +98,75 @@ def wave_timeline():
     return waves
 wave_timeline()
 
-# Create a dictionary with the average wave height for each day
+# Create a dictionary with the average wave height FOR EACH (WEEK?) DAY
+# Step One: make a list of Sunday waves, Monday waves, etc.
 
 
-def avg_wave_height():
-    wave_list = (collect_waves())
-    avg_wave = sum(wave for wave in wave_list if wave)/len(wave_list)
-    print(avg_wave)
+# def avg_daily_wave_height():
+    # wave_list = wave_timeline()
+
+    #daily_waves = {}
+
+    #sunday_waves = sum[]/5
+    #monday_waves = sum[]/5
+    #tuesday_waves = sum[]/4
+    #wednesday_waves = sum[]/4
+    #thursday_waves = sum[]/4
+    #friday_waves = sum[]/4
+    #saturday_waves = sum[]/5
+
+    #total_waves = sum(floated_waves)
+    #avg_wave = total_waves/len(wave_list)
+    #print(avg_wave)
+    #return avg_wave
 # avg_wave_height()
 
+def waves_per_day(): # this coding authored by Joel Taddei taddeimania
+    from enum import Enum
+
+    class DayOfWeek(Enum):
+        Sunday = 0
+        Monday = 1
+        Tuesday = 2
+        Wednesday = 3
+        Thursday = 4
+        Friday = 5
+        Saturday = 6
+
+    def get_day_of_week(year, month, day):
+        month_table = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
+        year -= 1 if month < 3 else 0
+        return DayOfWeek(int((year + year / 4 - year / 100 + year / 400 + month_table[month - 1] + day) % 7))
+
+    print(get_day_of_week(1990, 4, 2))
+    return get_day_of_week(1990, 4, 2)
+# waves_per_day()
 
 # Create a nested comprehension to get the average of the Homework 1 grades.
+
+
+def homework_average():
+    grade_dict = {
+        'Gale': {'Homework 1': 88, 'Homework 2': 76},
+        'Jordan': {'Homework 1': 92, 'Homework 2': 87},
+        'Peyton': {'Homework 1': 84, 'Homework 2': 77},
+        'River': {'Homework 1': 85, 'Homework 2': 91},
+        }
+    homework_one = []
+    names = grade_dict.keys()
+
+    for names in grade_dict:
+        this = (grade_dict.get(names, {}).get('Homework 1'))
+        homework_one.append(this)
+
+    #one = (grade_dict.get(key, {}).get('Homework 1'))
+
+    #homework = grade_dict.values()
+    #homework_one = {'Homework 1': value for key, value in grade_dict.values()}
+
+
+    # print(names)
+    #print(homework)
+    print(this)
+homework_average()
+# avg_homework_one = {header: input_set[index] for name, homework in enumerate(headers)}
